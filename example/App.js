2990d5d2f8a69e3352f2e68b067c1dffe04a23cc
Simo Aleksandrov <simo3003@me.com>
2020-02-10 16:17:22 +0200

    navigation 5.0

--- example/App.js
+++ example/App.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import App from "./navigation/AppNavigation";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-export default class App extends Component {
-  render() {
-    return <View style={styles.container}>test</View>;
-  }
-}
+export default () => (
+  <NavigationContainer>
+    <App />
+  </NavigationContainer>
+);
 
 const styles = StyleSheet.create({
   container: {
---
+++ example/navigation/AppNavigation.js
@@ -0,0 +1,103 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Text } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import styled from "styled-components/native";
+// import { TabBar } from 'react-native-animated-nav-tab-bar'
+
+const Screen = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f2f2;
+`;
+
+const TabBarIcon = props => {
+  return (
+    <Icon
+      name={props.name}
+      size={props.size ? props.size : 24}
+      color={props.focused ? props.tintColor : "#222222"}
+    />
+  );
+};
+
+const Home = () => (
+  <Screen>
+    <Text>Home</Text>
+  </Screen>
+);
+
+const Discover = () => (
+  <Screen>
+    <Text>Discover</Text>
+  </Screen>
+);
+
+const Images = () => (
+  <Screen>
+    <Text>Images</Text>
+  </Screen>
+);
+
+const Profile = () => (
+  <Screen>
+    <Text>Profile</Text>
+  </Screen>
+);
+
+//     {
+//     tabBarOptions: {
+//         activeTintColor: "#2F7C6E",
+//         inactiveTintColor: "#222222",
+//     },
+
+//     tabBarComponent: props => <TabBar
+//         activeColors={'#2F7C6E'}
+//         activeTabBackgrounds={'#DFF7F6'}
+//         {...props}
+//     />,
+// }
+
+const Tabs = createBottomTabNavigator();
+
+export default () => (
+  <Tabs.Navigator>
+    <Tabs.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarIcon: ({ focused, tintColor }) => (
+          <TabBarIcon focused={focused} tintColor={tintColor} name="home" />
+        )
+      }}
+    />
+    <Tabs.Screen
+      name="Discover"
+      component={Discover}
+      options={{
+        tabBarIcon: ({ focused, tintColor }) => (
+          <TabBarIcon focused={focused} tintColor={tintColor} name="search" />
+        )
+      }}
+    />
+    <Tabs.Screen
+      name="Images"
+      component={Images}
+      options={{
+        tabBarIcon: ({ focused, tintColor }) => (
+          <TabBarIcon focused={focused} tintColor={tintColor} name="image" />
+        )
+      }}
+    />
+    <Tabs.Screen
+      name="Profile"
+      component={Profile}
+      options={{
+        tabBarIcon: ({ focused, tintColor }) => (
+          <TabBarIcon focused={focused} tintColor={tintColor} name="user" />
+        )
+      }}
+    />
+  </Tabs.Navigator>
+);
