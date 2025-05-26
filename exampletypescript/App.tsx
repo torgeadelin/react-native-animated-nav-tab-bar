f15b71d730a9794c50f4919bc90495340d8f215c
torgeadelin <torgeadelin@gmail.com>
2025-05-26 18:14:27 +0100

    feat(rn-upgrade): attempt upgrade manual

--- exampletypescript/App.tsx
+++ exampletypescript/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigation from './navigation/AppNavigation';
 
 const App = () => {
--- exampletypescript/metro.config.js
+++ exampletypescript/metro.config.js
@@ -1,4 +1,14 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const path = require("path");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { resolver: { sourceExts, assetExts } } = defaultConfig;
 
 /**
  * Metro configuration
@@ -6,6 +16,34 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"],
+    unstable_enablePackageExports: false,
+    resolveRequest: function packageExportsResolver(context, moduleImport, platform) {
+      // Use the browser version of the package for React Native 
+      if (moduleImport === '<package>' || moduleImport.startsWith('<package>/')) {
+        return context.resolveRequest(
+          {
+            ...context,
+            unstable_conditionNames: ['browser'],
+          },
+          moduleImport,
+          platform,
+        );
+      }
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+      // Fall back to normal resolution
+      return context.resolveRequest(context, moduleImport, platform);
+    }
+  },
+  watchFolders: [path.resolve(__dirname, "../")],
+};
+
+
+
+module.exports = mergeConfig(defaultConfig, config);
